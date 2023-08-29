f225418cb43311b33c409d7bbb5774cd84495ed5
DariaBoreysha <125185895+DariaBoreysha@users.noreply.github.com>
2023-08-29 20:16:41 +0300

    Update SignIpPage.java

--- src/main/java/Pages/SignIpPage.java
+++ src/main/java/Pages/SignIpPage.java
@@ -4,8 +4,8 @@ import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$x;
 
-public class SignIpPage extends MainPage{
-    MainPage m = new MainPage();
+public class SignIpPage {
+
 
     protected static SelenideElement createAnAccountLink = $x(".//div[@class='col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2']//p/a");
 
